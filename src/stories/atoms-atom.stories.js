a4fde9f58a8574ceeb00f50744e663029977a91d
YumekaSugawara <yumeharu873@gmail.com>
2022-07-12 13:32:21 +0900

    atomsbuttonの作成２

---
+++ src/stories/atoms-atom.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from '../components/atoms/Button/button.html'
+import '../components/atoms/Button/button.scss'
+
+export default { title: 'Atoms/Button' }
+
+export const Example = () => Item_Example
